78d884e84d7dc10c0296a3ec01033abd3d8fb8d1
Reyhaneh-developer <r9361250@gmail.com>
2026-02-11 11:38:43 +0330

    compelet ferant end search and filter

--- client/src/app/services/member.service.ts
+++ client/src/app/services/member.service.ts
@@ -6,6 +6,7 @@ import { environment } from '../../environments/environment.development';
 import { PaginationParams } from '../models/helpers/paginationParams.model';
 import { PaginatedResult } from '../models/helpers/paginatedResult';
 import { PaginationHandler } from '../extensions/paginationHandler';
+import { MemberParams } from '../models/helpers/member-params';
 
 @Injectable({
   providedIn: 'root'
@@ -16,16 +17,28 @@ export class MemberService {
   private readonly _baseApiUrl: string = environment.apiUrl + 'api/member/';
   private _paginationHandler = new PaginationHandler();
 
-  getAllMembers(paginationParams: PaginationParams): Observable<PaginatedResult<Member[]>> {
-    let params = new HttpParams();
-
-    params = params.append('pageSize', paginationParams.pageSize);
-    params = params.append('pageNumber', paginationParams.pageNumber);
+  getAllMembers(memberParams: MemberParams): Observable<PaginatedResult<Member[]>> {
+    const params = this.getHttpParams(memberParams);
 
     return this._paginationHandler.getPaginatedResult<Member[]>(this._baseApiUrl + 'get-all', params);
   }
 
   getByUserName(userNameInput: string): Observable<Member | undefined> {
     return this.http.get<Member>(this._baseApiUrl + 'get-by-username/' + userNameInput);
+  }
+
+  private getHttpParams(memberParams: MemberParams): HttpParams {
+    let params = new HttpParams();
+
+    if(memberParams){
+        params = params.append('search', memberParams.search);
+      params = params.append('pageSize', memberParams.pageSize);
+      params = params.append('pageNumber', memberParams.pageNumber);
+      params = params.append('orderBy', memberParams.orderBy);
+      params = params.append('minAge', memberParams.minAge);
+      params = params.append('maxAge', memberParams.maxAge);
+    }
+
+    return params;
   }
 }
